aa8d3e0682766d424f04ddeefc25a4d991a60b3e
Antoine GARNIER <antoinegarnier@hotmail.fr>
2017-01-18 15:38:14 +0100

    Architecture de base

---
+++ ChiffrementRSA/src/com/univangers/m2sili/chiffrement/Key.java
@@ -0,0 +1,21 @@
+package com.univangers.m2sili.chiffrement;
+
+import java.math.BigInteger;
+
+/**
+ * Class that represents a key
+ */
+public class Key {
+    private BigInteger n;
+    private BigInteger e;
+    
+    public Key(){
+        n= new BigInteger("100000000000000");
+        e= new BigInteger("100000000000000");
+    }
+    public Key(BigInteger a, BigInteger b){
+        n= a;
+        e= b;
+    };
+}
+	
