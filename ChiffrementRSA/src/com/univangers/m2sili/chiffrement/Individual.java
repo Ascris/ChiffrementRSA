f91a84d57e6360cbe4a7ec24fc97572343753836
jdefaye <johan.defaye@hotmail.fr>
2017-01-18 17:07:56 +0100

    Création de la classe Server Début de la communication Server/Individual

--- ChiffrementRSA/src/com/univangers/m2sili/chiffrement/Individual.java
+++ ChiffrementRSA/src/com/univangers/m2sili/chiffrement/Individual.java
@@ -1,13 +1,38 @@
 package com.univangers.m2sili.chiffrement;
 
-
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.*;
 
 /**
- * Represent an individual who can send a crypted message to another individual
+ * Represent an individual who can send a crypted message to another individual thanks to the Server class
  * Use the RSA encryption to communicate safely
  */
 public class Individual {
-    private String name;
     
+    private String _name;
+    private Key _publicKey;
+    private Key _privateKey;
+    private BufferedReader in;
+    private PrintWriter out;
+    
+    public Individual(String name, Key publicK, Key privateK) {
+        _name = name;
+        _publicKey = publicK;
+        _privateKey = privateK;
+    }
+
+    public static void main(String[] args){
+        
+        Socket socket;
+        try {
+            System.out.println("Connexion au serveur au port " + Server.PORTNUMBER + " ...");
+            socket = new Socket("localhost", Server.PORTNUMBER);
+            System.out.println("Connexion accepté !");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
