18199f4ba4523585df908c5412cd884eb09fcf7f
jdefaye <johan.defaye@hotmail.fr>
2017-01-18 15:45:13 +0100

    merge

---
+++ ChiffrementRSA/src/com/univangers/m2sili/chiffrement/Individual.java
@@ -0,0 +1,13 @@
+package com.univangers.m2sili.chiffrement;
+
+
+import java.math.BigInteger;
+
+/**
+ * Represent an individual who can send a crypted message to another individual
+ * Use the RSA encryption to communicate safely
+ */
+public class Individual {
+    private String name;
+    
+}
